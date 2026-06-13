34f224d67f5bc24ba41f3342d7fa41f1ec6177d9
Jgocunha <gasparcunha19@outlook.pt>
2026-06-13 08:26:48 +0100

    fix(tests): use explicit ElementDimensions{} in push_back to fix MSVC C2665

--- neat-dnfs/tests/test_helpers.h
+++ neat-dnfs/tests/test_helpers.h
@@ -7,9 +7,9 @@ inline SolutionTopology makeTopology(int inputs, int outputs, int hidden = 0)
 {
     using dnf_composer::element::ElementDimensions;
     std::vector<std::pair<FieldGeneType, ElementDimensions>> genes;
-    for (int i = 0; i < inputs;  ++i) genes.push_back({FieldGeneType::INPUT,  {100, 1.0}});
-    for (int i = 0; i < outputs; ++i) genes.push_back({FieldGeneType::OUTPUT, {100, 1.0}});
-    for (int i = 0; i < hidden;  ++i) genes.push_back({FieldGeneType::HIDDEN, {100, 1.0}});
+    for (int i = 0; i < inputs;  ++i) genes.push_back({FieldGeneType::INPUT,  ElementDimensions{100, 1.0}});
+    for (int i = 0; i < outputs; ++i) genes.push_back({FieldGeneType::OUTPUT, ElementDimensions{100, 1.0}});
+    for (int i = 0; i < hidden;  ++i) genes.push_back({FieldGeneType::HIDDEN, ElementDimensions{100, 1.0}});
     return SolutionTopology(genes);
 }
 
